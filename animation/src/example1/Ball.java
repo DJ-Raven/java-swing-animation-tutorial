633c4817ffbe2ff3ff2cc6de11d5c226ebd65ae7
laing raven <laingraven.java@gmail.com>
2022-01-06 21:42:22 +0700

    first commit

---
+++ animation/src/example1/Ball.java
@@ -0,0 +1,45 @@
+package example1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+
+public class Ball {
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Dimension getSize() {
+        return size;
+    }
+
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public Ball(Color color, Dimension size, Point location) {
+        this.color = color;
+        this.size = size;
+        this.location = location;
+    }
+
+    public Ball() {
+    }
+
+    private Color color;
+    private Dimension size;
+    private Point location;
+}
